9b7caed28be364c52a98cb2695db09b3ebd771fb
GustavoBryan <devgustavob@gmail.com>
2023-03-28 13:19:17 -0300

    conta poupança

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -2,6 +2,6 @@ import { Conta } from "./Conta";
 
 export class ContaPoupanca extends Conta {
     constructor(saldoInicial, cliente, agencia) {
-        super(saldoInicial, cliente, agencia)
+        super(saldoInicial, cliente, agencia);
     }
 }
