75da4904e3830b71b2136f41ade70904855704a0
GustavoBryan <devgustavob@gmail.com>
2023-03-30 20:12:34 -0300

    autenticação

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 diretor.cadastrarSenha("123456");
-
 const gerente = new Gerente("Ricardo", 5000, 12345678903);
 gerente.cadastrarSenha("1234");
 
-//const estaLogado = SistemaAutenticacao.login(diretor, "123456");
-const estaLogado = SistemaAutenticacao.login(gerente, "1234");
+const cliente = new Cliente ("Lais", 87654312345, "4567");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "4567");
 
-
-console.log(estaLogado);
+console.log(clienteEstaLogado,diretorEstaLogado,gerenteEstaLogado);
